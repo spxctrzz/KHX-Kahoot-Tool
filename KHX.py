61dbc2a00ceca41369e5e4219c0c268aaeaeb6db
spxctrzz <76263277+spxctrzz@users.noreply.github.com>
2021-04-25 16:33:52 -0400

    Added long ascii spammer + made bots count in client more accurate to ingame
    so basically made it so that when you want to add 100 bots, it will add right around 100 instead of 88, 112 etc

--- KHX.py
+++ KHX.py
@@ -11,6 +11,7 @@ import string
 bot = client()
 
 def intro():
+    os.system('cls')
     print(Fore.LIGHTMAGENTA_EX, """
   | | |  ╦╔═  ╦ ╦  ═╗ ╦
   | | |  ╠╩╗  ╠═╣  ╔╩╦╝
@@ -21,26 +22,53 @@ def intro():
     print(Fore.LIGHTMAGENTA_EX, ' |')
 
 def bot_game():
-    os.system('cls')
     intro()
     random_name_choice = input("  | Random Bot Name? [Y/N] > ")
 
-    if random_name_choice == 'y':
-        bots_name = ''.join((random.choice(string.ascii_lowercase) for num in range(11)))
+    if random_name_choice == 'y' or 'Y' or 'yes' or 'YES':
+        bots_name = ''.join((random.choice(string.ascii_lowercase) for num in range(7)))
     else:
         bots_name = input("  | Bots Name > ")
 
     bots_amount = int(input("  | How Many Bots? > "))
     code = input("  | Kahoot Code > ")
-    x = 0
+    x = -12
     while x <= bots_amount:
         try:
             bot.join(code, bots_name + " - " + str(x))
-            print('    | Sending bots: ' + str(x))
+            if x >= 0:
+                print('    | Sending bots: ' + str(x))
         except:
             print("    |Sorry, Bots Failed.")
             sleep(1)
-            os.system('cls')
+            beginning()
+        finally:
+            x += 1
+    print(Fore.BLUE, '    | All Bots Successfully Sent ')
+    sleep(1)
+    beginning()
+
+def ascii_name_spammer():
+    intro()
+    repeating_word_choice = input("  | Have Name be Repeating Word? [Y/N] > ")
+    if repeating_word_choice == 'y' or 'Y' or 'yes' or 'YES':
+        bots_name_raw = input("  | Bots Name > ")
+        bots_name = (bots_name_raw*100)
+    else:
+        bots_name = random_ascii_name = ''.join(
+            (random.choice(string.ascii_lowercase + string.punctuation + string.digits) for num in range(100)))
+
+    bots_amount = int(input("  | How Many Bots? > "))
+    code = input("  | Kahoot Code > ")
+    x = -20
+    while x <= bots_amount:
+        try:
+            bot.join(code, bots_name + " - " + str(x))
+            if x >= 0:
+                print('    | Sending bots: ' + str(x))
+        except:
+            print("    |Sorry, Bots Failed.")
+            sleep(1)
             beginning()
         finally:
             x += 1
@@ -49,11 +77,13 @@ def bot_game():
     beginning()
 
 
+
 def start():
-    menuchoice = input("  | [1] Flood Game\n  |\n  | > ")
+    menuchoice = input("""  | [1] Flood Game\n  | [2] Long ASCII Name Flooder\n  | >  """)
     if menuchoice == '1':
         bot_game()
-
+    if menuchoice == '2':
+        ascii_name_spammer()
 
 def beginning():
     intro()
